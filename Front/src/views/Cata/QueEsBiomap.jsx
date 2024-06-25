09ffa90ba10bcd9d6788a92e922c4b2f9e20df89
cata-maturana <catalina.maturana@genstudents.org>
2024-06-25 11:07:31 -0400

    Agrego vista - Que es biomap

---
+++ Front/src/views/Cata/QueEsBiomap.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+
+const QueEsBiomap = () => {
+  return (
+    <div className="container mt-5">
+      <div className="row">
+        <div className="col-12 col-md-6 offset-md-3">
+          <div className="text-center">
+          <h1 class="text-center mt-5 pt-5">
+          ¿QUÉ ES BIOMAP?
+        </h1>
+            <p>
+              Es una plataforma que busca educar a través de un mapa interactivo que muestra los distintos tipos de ecosistemas de Chile y su principal biodiversidad de flora y fauna. Su objetivo es ser una herramienta educativa orientada a concientizar y difundir sobre la conservación y protección de las especies nativas, enfatizando aquellas que están en peligro de extinción como también las que son poco conocidas.
+            </p>
+            <p>
+              Biomap enseña a través de cápsulas informativas la biodiversidad en el país, y cómo esta se distribuye en los diferentes biomas que componen cada región. La página pretende ser de uso amigable para facilitar la experiencia de interactividad con el usuario, permitiendo así que sea apta para todo público.
+              <img src="https://lh3.google.com/u/0/d/19iqEbcH5xE1gYQ8zeNx0MgU0bmTPdfhT=w1920-h868-iv1"></img>  
+
+
+              
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default QueEsBiomap;
